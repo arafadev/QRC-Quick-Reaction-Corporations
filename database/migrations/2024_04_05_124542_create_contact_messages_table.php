d1f9a3f86f3fd7eb4c11b817cd47fa70b6800cb0
Arafa Dev <arafa.dev@gmail.com>
2024-04-05 15:52:12 +0200

    contact messages migration

---
+++ database/migrations/2024_04_05_124542_create_contact_messages_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contact_messages', function (Blueprint $table) {
+            $table->id();
+            $table->text('message');
+            $table->string('phone', 20);
+            $table->string('email', 100);
+            $table->unsignedBigInteger('contactable_id');
+            $table->string('contactable_type');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contact_messages');
+    }
+};
